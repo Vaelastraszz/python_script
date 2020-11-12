ac5dcf93e1da31b372c92057a0d3bd574399222f
RomainLejeune <romain.lejeun@gmail.com>
2020-11-12 22:33:29 +0100

    final commit

--- coco_data/to_coco.py
+++ coco_data/to_coco.py
@@ -1,7 +1,8 @@
-import json
 import csv
+import json
+import os
+import glob
 from PIL import Image
-import _locale
 
 
 class Coco:
@@ -9,6 +10,7 @@ class Coco:
         self.document = self.load_document
         self.language = self.create_language
         self.coco = {}
+        self.hash_img = {}
         self.coco["info"] = {}
         self.coco["licenses"] = [{}]
         self.coco["images"] = []
@@ -38,15 +40,55 @@ class Coco:
             food_dic = {row[0]: row[2] for row in food_inv}
         return food_dic
 
-    def get_size(self):
-        for k in self.document.keys():
-            im = Image.open("assignement_imgs/" + k)
-            width, height = im.size
+    @property
+    def create_imgs(self):
+        for i, filename in enumerate(glob.iglob("assignment_imgs/*.j*")):
+            temp_dict = {}
+            img = Image.open(
+                "/Users/RomainLejeune/Desktop/python_script/coco_data/" + filename
+            )
+            width, height = img.size
 
-    def transform(self):
-        pass
+            if filename not in self.hash_img:
+                self.hash_img[filename.split("/")[-1]] = i
+
+            temp_dict["id"] = i
+            temp_dict["file_name"] = filename.split("/")[-1]
+            temp_dict["width"] = width
+            temp_dict["height"] = height
+
+            self.coco["images"].append(temp_dict)
+
+    @property
+    def create_categ(self):
+        for key, value in self.language.items():
+            temp_dict = {}
+            temp_dict["id"] = key
+            temp_dict["name"] = value
+            temp_dict["supercategory"] = "none"
+            self.coco["categories"].append(temp_dict)
+
+    @property
+    def create_annotations(self, i=0):
+        for key, value in self.document.items():
+            for element in value:
+                if element["is_background"] is False:
+                    temp = {}
+                    temp["id"] = i
+                    temp["image_id"] = self.hash_img[key]
+                    temp["category_id"] = element["id"]
+                    temp["bbox"] = element["box"]
+                    temp["area"] = element["box"][-1] * element["box"][-2]
+                    temp["segmentation"] = []
+                    temp["iscrowd"] = 0
+                    self.coco["annotations"].append(temp)
+                    i += 1
 
 
 if __name__ == "__main__":
-    test = Coco()
-    print(print(test.language))
+    prout = Coco()
+    prout.create_imgs
+    prout.create_categ
+    prout.create_annotations
+    with open("data.json", "w") as fp:
+        json.dump(prout.coco, fp)
